6febbe6c86124865022ebcac2f4c7857147d8922
hvtienprotv84 <dmx031117@gmail.com>
2024-07-30 14:38:33 +0700

    push skills

--- pages/skills.vue
+++ pages/skills.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
-    <p>ABC</p>
+  <p class="text-5xl mb-[30px]">Skills.</p>
+  <div class="flex flex-wrap gap-2 max-w-[800px]">
+    <span v-for="(technology, index) in skills.technology" :key="index"
+      class="bg-neutral-200 rounded-full px-2 text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
+      {{ technology }}
+    </span>
   </div>
 </template>
 
 <script setup lang="ts">
+
+import skills from "~/assets/data/skills.json";
+
 useHead({
   title: "Tiến - Portfolio - NuxtJS | Skills",
   meta: [
@@ -14,5 +21,6 @@ useHead({
     },
   ],
 });
+
 
 </script>
